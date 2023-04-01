882396b0467a1649413a41d52f1ac1a5ebdbb506
ricajust <ricartoonstudio@gmail.com>
2023-04-01 17:00:45 -0300

    {ricajust}[finansys] - adding category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+import { Category } from './category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath:string = "api/categories";
+
+  constructor(private http:HttpClient) { }
+
+  public getAll(): Observable<Category[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategories)
+    );
+  }
+
+  public getById(id: number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    );
+  }
+
+  public create(category: Category): Observable<Category> {
+    return this.http.post(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    );
+  }
+
+  public update(category: Category): Observable<Category> {
+    const url = `${this.apiPath}/${category.id}`;
+    return this.http.put(url, category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+      );
+    }
+
+  public delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  private handleError(error: any): Observable<any>{
+    console.log("ERRO NA REQUISIÇÃO: ", error);
+    return throwError(error);
+  }
+
+  private jsonDataToCategory(jsonData:any): Category {
+    return jsonData as Category;
+  }
+
+  private jsonDataToCategories(jsonData:any[]): Category[] {
+    const categories: Category[] = [];
+
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+
+}
