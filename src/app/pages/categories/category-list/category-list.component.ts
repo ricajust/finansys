db10ccfb8f22813ec72b7fae4c685e3677ecae5d
ricajust <ricartoonstudio@gmail.com>
2023-04-02 10:25:54 -0300

    {ricajust}[finansys] - improving catetegory list component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  public categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      (categories) => this.categories = categories,
+      (error) => alert('Erro ao carregar lista!')
+    );
+  }
+
+  public deleteCategory(category): void {
+    const mustDelete = confirm("Deseja realmente excluir esse item?")
+    if(mustDelete){
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(elem => elem != category),
+        (error) => console.log("Erro ao tentar excluir", error)
+      );
+    }
   }
 
 }
