51df02258544d4e17b2507cf9864caf6d0b27e17
Ricardo Justino <ricartoonstudio@gmail.com>
2023-04-09 10:52:54 -0300

    {ricajust}[finasys] - addind association between Entry and Category

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
 import { Entry } from './entry.model';
+import { CategoryService } from '../../categories/shared/category.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class EntryService {
 
   private apiPath:string = "api/entries";
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private categoryService: CategoryService) { }
 
   public getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -29,18 +30,31 @@ export class EntryService {
   }
 
   public create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
-    );
+    return this.categoryService.getById(entry.categoryId).pipe(
+      //Retorna o objeto e não um Observable
+      flatMap(category => {
+        entry.category = category;
+        //Retorna um observable contendo um Entry Observable<Entry>
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        );
+      })
+    )
+
   }
 
   public update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
-      );
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+          );
+      })
+    );
     }
 
   public delete(id: number): Observable<any> {
